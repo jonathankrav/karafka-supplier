b3bd493b06d8099998388bd5eec3f0092fdfb0f5
jonathankrav <jk.fcdk@gmail.com>
2024-03-21 00:43:15 +0200

    with mono

--- karafka-supplier/src/main/java/telran/java51/KarafkaSupplierApplication.java
+++ karafka-supplier/src/main/java/telran/java51/KarafkaSupplierApplication.java
@@ -1,10 +1,12 @@
 package telran.java51;
 
 import java.util.function.Supplier;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @SpringBootApplication
 public class KarafkaSupplierApplication {
@@ -13,9 +15,15 @@ public class KarafkaSupplierApplication {
 		SpringApplication.run(KarafkaSupplierApplication.class, args);
 	}
 	
+//	@Bean
+//	Supplier<String> log() {
+//		return () -> new String("42");
+//	}
+	
 	@Bean
-	Supplier<String> log() {
-		return () -> new String("42");
+	Supplier<Mono<String>> log() {
+	    return () -> Mono.just("Hello from Supplier2")
+	                    .subscribeOn(Schedulers.boundedElastic());
 	}
 
 }
